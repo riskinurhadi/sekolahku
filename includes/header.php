0db7deed265bfd81cbe2679ebd5a16c95d1ab3b4
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-11-23 00:23:06 +0700

    modified:   assets/css/style.css 	modified:   includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -272,24 +272,6 @@ if ($table_check && $table_check->num_rows > 0) {
                             <i class="bi bi-megaphone"></i> Informasi Akademik
                         </a>
                     </li>
-                    <li class="has-dropdown">
-                        <a href="#hasilSubmenu" data-bs-toggle="collapse" aria-expanded="<?php echo (basename($_SERVER['PHP_SELF']) == 'hasil_latihan.php' || basename($_SERVER['PHP_SELF']) == 'presensi.php') ? 'true' : 'false'; ?>" class="dropdown-toggle <?php echo (basename($_SERVER['PHP_SELF']) == 'hasil_latihan.php' || basename($_SERVER['PHP_SELF']) == 'presensi.php') ? 'active' : ''; ?>">
-                            <i class="bi bi-check-circle"></i> Hasil
-                            <i class="bi bi-chevron-down dropdown-arrow"></i>
-                        </a>
-                        <ul class="collapse list-unstyled <?php echo (basename($_SERVER['PHP_SELF']) == 'hasil_latihan.php' || basename($_SERVER['PHP_SELF']) == 'presensi.php') ? 'show' : ''; ?>" id="hasilSubmenu">
-                            <li>
-                                <a href="<?php echo getBasePath(); ?>dashboard/siswa/hasil_latihan.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'hasil_latihan.php' ? 'active' : ''; ?>">
-                                    <i class="bi bi-file-earmark-check"></i> Hasil Latihan
-                                </a>
-                            </li>
-                            <li>
-                                <a href="<?php echo getBasePath(); ?>dashboard/siswa/presensi.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'presensi.php' ? 'active' : ''; ?>">
-                                    <i class="bi bi-clipboard-check"></i> Rekap Presensi
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
                     <li>
                         <a href="<?php echo getBasePath(); ?>dashboard/siswa/profil.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'profil.php' ? 'active' : ''; ?>">
                             <i class="bi bi-person-circle"></i> Profil
