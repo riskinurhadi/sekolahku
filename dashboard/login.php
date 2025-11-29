7f9338eded8c54c3a0518458f08065d173b74183
riskinurhadi <rizkibinmangtrisno@gmail.com>
2025-11-29 14:47:44 +0700

    modified:   dashboard/login.php

--- dashboard/login.php
+++ dashboard/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once 'config/database.php';
-require_once 'config/session.php';
+require_once '../config/database.php';
+require_once '../config/session.php';
 
 // Jika sudah login, redirect ke dashboard sesuai role
 if (isLoggedIn()) {
